8156c14ded3f54cf7fa332ae250b9a757071a98a
olee2 <65301179+olee2@users.noreply.github.com>
2020-06-27 21:28:57 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+from game import Game
+
+phrases = ["cat in hat", "man with gun", "good looking fellow", "lots of heads", "crazy looking bird", "the stoic prick"]
+
+if __name__ == "__main__":
+    game = Game(phrases)
+    game.game_loop()
+
+
+    
