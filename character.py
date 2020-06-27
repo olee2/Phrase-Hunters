62f3143623834780facc4fa5f7d3e7609da2b596
olee2 <65301179+olee2@users.noreply.github.com>
2020-06-27 21:29:00 +0200

    Create character.py

---
+++ character.py
@@ -0,0 +1,28 @@
+import string
+
+class Character(str):
+
+    def __init__(self, char):
+        self.was_guessed = False
+
+        if isinstance(char, str) and len(char) == 1 and (char in string.ascii_letters or char ==" "):
+            self.original = char
+        else:
+            raise ValueError("A character can only be one letter or a whitespace in string format.") 
+
+    def guess(self, guess):
+        if guess == self.original:
+            self.was_guessed = True
+            
+    def show(self):
+        if self.original == " ":
+            return (self.original)
+        elif self.was_guessed:
+            return (self.original)
+        return ("_")
+
+
+
+    
+        
+        
