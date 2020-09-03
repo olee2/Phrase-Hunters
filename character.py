f211fddcbcf17a878fb8c3797218f65ede160c0c
olee2 <65301179+olee2@users.noreply.github.com>
2020-09-03 10:40:30 +0200

    Update character.py

--- character.py
+++ character.py
@@ -1,18 +1,34 @@
 import string
 
 class Character(str):
+    """
+    An instance of the character class is created for every letter and
+    whitespace in the phrase. The class holds the character and information
+    of wether or not the character have been guessed.
+    """
 
     def __init__(self, char):
+        
         self.was_guessed = False
 
-        if isinstance(char, str) and len(char) == 1 and (char in string.ascii_letters or char ==" "):
+        if (isinstance(char, str) and len(char) == 1 and
+            (char in string.ascii_letters or char ==" ")):
             self.original = char
-        else:
-            raise ValueError("A character can only be one letter or a whitespace in string format.") 
             
+        else:
+            raise ValueError("A character can only be one",
+                             "letter or a whitespace in string format.")
+      
     def show(self):
+        """
+        Returns the character if it is a whitespace or if it was guessed,
+        else it will return an underscore.
+        """
+    
         if self.original == " ":
             return (self.original)
+        
         elif self.was_guessed:
             return (self.original)
+        
         return ("_")
