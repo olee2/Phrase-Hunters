180b19401882431c2302e1c10b498c8d251d22eb
olee2 <65301179+olee2@users.noreply.github.com>
2020-09-03 10:40:37 +0200

    Update phrase.py

--- phrase.py
+++ phrase.py
@@ -1,19 +1,23 @@
 from character import Character
 
 class Phrase():
+    """
+    The class takes a string phrase and creates a list of Character instances.
+    """
 
     def __init__(self, phrase):
-        self.phrase = []
-
-        for char in list(phrase):
-            self.phrase.append(Character(char.lower()))
-        
+        self.phrase = [Character(char.lower()) for char in list(phrase)]
 
     def __iter__(self):
         yield from self.phrase
 
-        
     def check(self):
+        """
+        Checks if the full phrase has been guessed and, if so,
+        set the was.guessed attribute to false for all instances of Character
+        in the phrase, in case the same phrase will be picked again.
+        """
+        
         if "_" in self.dynamic_phrase:
             return False
 
@@ -24,14 +28,18 @@ class Phrase():
         return True
 
     def display(self):
+        """ Returns a string of the list returned from the dynamic method. """
+    
         print ("\n"+"".join(self.dynamic_phrase))
 
-
     def dynamic(self):
-        self.dynamic_phrase = []
+        """
+        Creates a list where Character.show() is called on all
+        instances of Character in self.phrase.
+        """
+        
+        self.dynamic_phrase = [i.show() for i in self.phrase]
 
-        for i in self.phrase:
-            self.dynamic_phrase.append(i.show())
 
 
 
